bef3994696ab28b07bf4cbc207f7aae1f3dac4b0
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-23 23:14:28 -0500

    feat: updates users.controllers.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // GET      /users    or  /users?role=value
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
+
+  // GET      /users/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  // POST     /users
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  // PATCH    /users/:id/
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() user: {}) {
+    return { id, ...user };
+  }
+
+  // DELETE   /users/:id/
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return { id };
+  }
+}
