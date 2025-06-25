5bce38ee69105f1df35ef981eee393ad947a0b5c
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-06-24 22:18:29 -0500

    feat: adds createUser

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class UsersService {
   private users = [
-    { id: '1', name: 'Alice', email: 'alice@bigco.com', role: 'INTERN' },
-    { id: '2', name: 'Bob', email: 'bob@bigco.com', role: 'ENGINEER' },
-    { id: '3', name: 'Charlie', email: 'charlie@bigco.com', role: 'ADMIN' },
-    { id: '4', name: 'Dave', email: 'dave@bigco.com', role: 'ADMIN' },
-    { id: '5', name: 'Eve', email: 'eve@bigco.com', role: 'ENGINEER' },
+    { id: 1, name: 'Alice', email: 'alice@bigco.com', role: 'INTERN' },
+    { id: 2, name: 'Bob', email: 'bob@bigco.com', role: 'ENGINEER' },
+    { id: 3, name: 'Charlie', email: 'charlie@bigco.com', role: 'ADMIN' },
+    { id: 4, name: 'Dave', email: 'dave@bigco.com', role: 'ADMIN' },
+    { id: 5, name: 'Eve', email: 'eve@bigco.com', role: 'ENGINEER' },
   ];
 
   findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
@@ -17,7 +17,7 @@ export class UsersService {
     return this.users;
   }
 
-  findUserById(id: string) {
+  findUserById(id: number) {
     const user = this.users.find((user) => user.id === id);
 
     return (
@@ -28,5 +28,23 @@ export class UsersService {
         role: 'UNKNOWN',
       }
     );
+  }
+
+  createUser(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const userByHighestId = [...this.users].sort((a, b) => {
+      return (b.id = a.id);
+    });
+
+    const newUser = {
+      id: userByHighestId[0].id + 1,
+      ...user,
+    };
+
+    this.users.push(newUser);
+    return newUser;
   }
 }
